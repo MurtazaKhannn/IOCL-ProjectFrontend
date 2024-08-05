a43d0f8f4761167d0960b39289ae3de5fed851ef
MurtazaKhannn <152141459+MurtazaKhannn@users.noreply.github.com>
2024-08-05 10:36:19 +0530

    commit

--- src/page/SavedDraft.jsx
+++ src/page/SavedDraft.jsx
@@ -3,8 +3,6 @@ import { useNavigate, useParams } from "react-router-dom";
 import { ClipLoader } from "react-spinners";
 
 const SavedDraft = () => {
-  // const apiUrl = process.env.REACT_APP_API_URL;
-
   const { userId } = useParams(); // Get userId from URL params
   const [drafts, setDrafts] = useState([]);
   const [loadingDelete, setLoadingDelete] = useState(false);
@@ -39,8 +37,7 @@ const SavedDraft = () => {
     }
   }, [userId]);
 
-  const handleDelete = async (draftId , draftType) => {
-    
+  const handleDelete = async (draftId, draftType) => {
     const isConfirmed = window.confirm("Do you really want to delete this draft?");
 
     if (isConfirmed) {
@@ -67,10 +64,9 @@ const SavedDraft = () => {
     }
   };
 
-  const handleDraftClick = (draftId , draftType) => {
+  const handleDraftClick = (draftId, draftType) => {
     setLoadingView((prev) => ({ ...prev, [draftId]: true }));
-    console.log(loadingView);
-    
+
     setTimeout(() => {
       setLoadingView((prev) => ({ ...prev, [draftId]: false }));
       navigate(`/${draftType}/${draftId}`);
@@ -80,34 +76,38 @@ const SavedDraft = () => {
   return (
     <div className="w-full min-h-[88.9vh] font-sans mt-5 bg-zinc-100">
       {drafts.length > 0 ? (
-        drafts.map((draft) => (
-          <div
-            key={draft._id}
-            className="p-5 flex flex-col w-3/4 gap-5 ml-5 bg-white rounded-md shadow-md mb-4"
-          >
-            <div>
-              <h2 className="text-2xl font-bold">{draft.formName}</h2>
-              <p className="font-semibold">Ref No &nbsp; : &nbsp; {draft.referenceNumber}</p>
-              <p>CreatedAt: {draft.createdAt ? draft.createdAt.split('T')[0] : 'Date not available'}</p>
-              <p>UpdatedAt: {draft.updatedAt ? draft.updatedAt.split('T')[0] : 'Date not available'}</p>
-            </div>
-            <div className="flex justify-between">
-              <button
-                onClick={() => handleDelete(draft._id , draft.formName)}
-                className="text-white text-xl bg-red-500 hover:bg-red-700 py-2 px-4 rounded-md"
+        <div className="w-full p-5">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
+            {drafts.map((draft) => (
+              <div
+                key={draft._id}
+                className="p-5 flex flex-col bg-white rounded-md shadow-md"
               >
-                {loadingDelete ? <ClipLoader size={20} color={"#fff"} /> : "Delete"}
-              </button>
+                <div className="mb-5">
+                  <h2 className="text-2xl font-bold">{draft.formName}</h2>
+                  <p className="font-semibold">Ref No &nbsp; : &nbsp; {draft.referenceNumber}</p>
+                  <p>CreatedAt: {draft.createdAt ? draft.createdAt.split('T')[0] : 'Date not available'}</p>
+                  <p>UpdatedAt: {draft.updatedAt ? draft.updatedAt.split('T')[0] : 'Date not available'}</p>
+                </div>
+                <div className="flex justify-between mt-auto">
+                  <button
+                    onClick={() => handleDelete(draft._id, draft.formName)}
+                    className="text-white text-xl bg-red-500 hover:bg-red-700 py-2 px-4 rounded-md"
+                  >
+                    {loadingDelete ? <ClipLoader size={20} color={"#fff"} /> : "Delete"}
+                  </button>
 
-              <button
-                onClick={() => handleDraftClick(draft._id , draft.formName)}
-                className="text-white text-xl bg-orange-500 hover:bg-orange-600 py-2 px-4 rounded-md"
-              >
-                {loadingView[draft._id] ? <ClipLoader  size={20} color={"#fff"} /> : "View"}
-              </button>
-            </div>
+                  <button
+                    onClick={() => handleDraftClick(draft._id, draft.formName)}
+                    className="text-white text-xl bg-orange-500 hover:bg-orange-600 py-2 px-4 rounded-md"
+                  >
+                    {loadingView[draft._id] ? <ClipLoader size={20} color={"#fff"} /> : "View"}
+                  </button>
+                </div>
+              </div>
+            ))}
           </div>
-        ))
+        </div>
       ) : (
         <p className="flex items-center justify-center w-full h-[88.9vh] text-5xl">No drafts available</p>
       )}
